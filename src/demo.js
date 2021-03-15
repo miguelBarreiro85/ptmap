fa8b858697f08584f95434aad35148f2c57eb2ee
miguelbarreiro85 <miguelbarreiro85@gmail.com>
2021-03-15 16:15:28 +0000

    1

---
+++ src/demo.js
@@ -0,0 +1,11 @@
+import 'jquery-mapael';
+import $ from 'jquery';
+import 'jquery-mapael/js/maps/world_countries.js';
+import './pt';
+
+
+$(".container").mapael({
+    map : {
+        name : "portugal"
+    }
+});
